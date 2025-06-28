9c590219179397a9dd32b65582f177ba6e90ea35
RajatYadav058 <rajattech4500@gmail.com>
2025-06-28 21:46:12 +0530

    Addon 1

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const Local_url = "mongodb://127.0.0.1:27017/CurdOperationMERNstack"
+const Live_url = "mongodb+srv://rajat:rajat123@cluster0.kqqoudf.mongodb.net/CurdApi?retryWrites=true&w=majority&appName=Cluster0"
 
 const dbconnect = async ()=>{
     try{
-        await mongoose.connect(Local_url)
+        await mongoose.connect(Live_url)
         console.log('mongodb connectd')
     }
     catch(error){
